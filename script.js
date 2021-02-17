c2417bac2bfe60a74a2d11c5154c4b910f8e8c4e
Lily Chaber <76661777+weaveress@users.noreply.github.com>
2021-02-17 15:45:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+
 const generateTarget = () => {
     let target = Math.floor(Math.random() * 9);
     return target;
