63a86b722861f51d37c75720f4cd787e1f395f15
Lily Chaber <76661777+weaveress@users.noreply.github.com>
2021-02-19 11:47:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 const generateTarget = () => {
-    let target = Math.floor(Math.random() * 9);
+    let target = Math.floor(Math.random() * 10);
     return target;
 };
 
