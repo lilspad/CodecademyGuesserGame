1a3d82f741476143833090d76d7eb13c62d15583
Lily Chaber <76661777+weaveress@users.noreply.github.com>
2021-02-24 19:07:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,26 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// Generating a random target number
 const generateTarget = () => {
     let target = Math.floor(Math.random() * 10);
     return target;
 };
 
+// the next 2 functions check the distances between the two guesses and the target, and compares them. 
+// if human guess is closer to the target (smaller distance) or if game ties (same distance), the human wins. 
 const getAbsoluteDistance = (x, y) => Math.abs(x - y);
 
 const compareGuesses = (humanGuessInput, computerGuess, target) => {
-    if (getAbsoluteDistance(humanGuessInput, target) < getAbsoluteDistance(computerGuess, target) || getAbsoluteDistance(humanGuessInput, target) === getAbsoluteDistance(computerGuess, target)) {
+    if (getAbsoluteDistance(humanGuessInput, target) < getAbsoluteDistance(computerGuess, target) || 
+        getAbsoluteDistance(humanGuessInput, target) === getAbsoluteDistance(computerGuess, target)) {
         return true;
     } else {
         return false;
     }
 };
 
+// adding 1 to score variable corresponding to the winner
 const updateScore = (winner) => {
     if (winner === 'human') {
         humanScore += 1;
@@ -27,6 +32,7 @@ const updateScore = (winner) => {
     }
 };
 
+//adding 1 to the round variable after each play through 
 function advanceRound() {
     currentRoundNumber += 1;
 };
